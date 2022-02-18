856ade956705777c7ab98f8a5ae611ed0fae89d8
vgj24 <vrushlijoshi24@gmail.com>
2022-02-18 15:17:51 +0530

    INdianCensusAnalyserCode

---
+++ IndianStateCensusAnalyzer/CSVAdapterFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndianStateCensusAnalyzer.DTO;
+
+namespace IndianStateCensusAnalyzer
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+
+}
