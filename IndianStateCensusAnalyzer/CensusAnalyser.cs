1b06855956e187a1b5a7150507928fe70664e770
vgj24 <vrushlijoshi24@gmail.com>
2022-02-18 15:22:35 +0530

    INdianCensusAnalyserCode

---
+++ IndianStateCensusAnalyzer/CensusAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndianStateCensusAnalyzer.DTO;
+
+namespace IndianStateCensusAnalyzer
+{
+    public class CensusAnalyser
+    {
+
+        public static int a = 10;
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            //CensusAnalyser obj = new CensusAnalyser();
+            //Console.WriteLine(obj.a);
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+
+
+}
